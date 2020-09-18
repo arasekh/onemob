9089eba13a67e095607099febd1e84788e8849cf
alirezaAsadi2018 <asady529@gmail.com>
2020-09-18 14:51:43 +0430

    remove all files from repo

--- backend/virtualclass/views.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.core.mail import send_mail
-from django.shortcuts import render
-
-def signinuser(request):
-    if request.method == 'GET':
-        return
-    elif request.method == 'POST':
-        pass
