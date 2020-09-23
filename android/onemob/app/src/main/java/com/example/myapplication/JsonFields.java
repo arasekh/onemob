0f0512d0477e16a9f698c0ab77cba0feac332830
Ali Asgary <58213046+Ali79Asgary@users.noreply.github.com>
2020-09-23 13:29:14 +0330

    In this version : android can connect to the server and read json file from database and show the information. also the ui section became better than past.

---
+++ android/onemob/app/src/main/java/com/example/myapplication/JsonFields.java
@@ -0,0 +1,102 @@
+package com.example.myapplication;
+
+//This class contains json fields which consist on the server.
+public class JsonFields {
+    int id = 0;
+    String username = "username";
+    String first_name = "first_name";
+    String last_name = "last_name";
+    String email = "email";
+    String password = "password";
+    String last_login = "last_login";
+    String date_joined = "date_joined";
+
+    public JsonFields(int id, String username, String first_name, String last_name, String email, String password, String last_login, String date_joined) {
+        this.id = id;
+        this.username = username;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.password = password;
+        this.last_login = last_login;
+        this.date_joined = date_joined;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLast_login() {
+        return last_login;
+    }
+
+    public void setLast_login(String last_login) {
+        this.last_login = last_login;
+    }
+
+    public String getDate_joined() {
+        return date_joined;
+    }
+
+    public void setDate_joined(String date_joined) {
+        this.date_joined = date_joined;
+    }
+
+    @Override
+    public String toString() {
+        return "JsonFields{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", last_login='" + last_login + '\'' +
+                ", date_joined='" + date_joined + '\'' +
+                '}';
+    }
+}
--- android/onemob/app/src/main/java/com/example/myapplication/MainActivityEmpty.java
+++ android/onemob/app/src/main/java/com/example/myapplication/MainActivityEmpty.java
@@ -10,38 +10,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivityEmpty extends AppCompatActivity{
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        Intent activityIntent;
+    Intent activityIntent;
 
-        int temp = 1;
+    int temp = 1;
 //        int temp = 2;
 //        int temp = 3;
 
 //        String token = null;
 
 
-        if (temp == 1) {
-            activityIntent = new Intent(this, MainActivity.class);
-            Log.d("", "-----------------------------------Main Activity launched ----------------------------------------------------------------------------------------------------");
-        } else if(temp == 2) {
-            activityIntent = new Intent(this, LoginActivity.class);
-            Log.d("", "-----------------------------------Login Activity launched --------------------------------------------------------------------------------------------------");
-        }
-        else {
-            activityIntent = new Intent(this, RegistrationActivity.class);
-            Log.d("", "-----------------------------------Registration Activity launched --------------------------------------------------------------------------------------------------");
-        }
+    if (temp == 1) {
+      activityIntent = new Intent(this, MainActivity.class);
+      Log.d("", "-----------------------------------Main Activity launched ----------------------------------------------------------------------------------------------------");
+    } else if(temp == 2) {
+      activityIntent = new Intent(this, LoginActivity.class);
+      Log.d("", "-----------------------------------Login Activity launched --------------------------------------------------------------------------------------------------");
+    }
+    else {
+      activityIntent = new Intent(this, RegistrationActivity.class);
+      Log.d("", "-----------------------------------Registration Activity launched --------------------------------------------------------------------------------------------------");
+    }
 //        if (Util.getToken() != null) {
 //            activityIntent = new Intent(this, MainActivity.class);
 //        } else {
 //            activityIntent = new Intent(this, LoginActivity.class);
 //        }
-        startActivity(activityIntent);
-        finish();
-    }
+    startActivity(activityIntent);
+    finish();
+  }
 
 
 }
