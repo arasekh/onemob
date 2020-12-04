161387c9133d08b98bfa14eab7d2451de1357124
alirezaAsadi2018 <asady529@gmail.com>
2020-12-04 20:09:07 +0330

    configure .env settings

--- backend/androidserver/settings.py
+++ backend/androidserver/settings.py
@@ -94,16 +94,24 @@ WSGI_APPLICATION = 'androidserver.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-	'ENGINE': 'django.db.backends.postgresql_psycopg2',
-	'NAME': env('DATABASE_NAME'),
-	'USER': env('DATABASE_USER'),
-	'PASSWORD': env('DATABASE_PASSWORD'),
-	'HOST': '127.0.0.1',
-	'PORT': '5432',
+if 'sqlite' in env('DATABASE'):
+    DATABASES = {
+	    'default': {
+	        'ENGINE': 'django.db.backends.sqlite3',
+	        'NAME': BASE_DIR / 'db.sqlite3',
+	    }
+	}
+else:
+    DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        }
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
