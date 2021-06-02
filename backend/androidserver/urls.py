d26a493c481e659afb0b1f6ce460ee2a968ab5ee
alirezaAsadi2018 <asady529@gmail.com>
2021-06-02 09:22:27 +0430

    remove additional auth for rest_framework, not needed anymore

--- backend/androidserver/urls.py
+++ backend/androidserver/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     # api
     path('api/', include(('virtualclass.api.urls', 'virtualclass'), namespace='api')),
     path('api/payment/', include(('payment.urls', 'payment'), namespace='payment_api')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
